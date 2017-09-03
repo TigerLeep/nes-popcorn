b306672f6b0e01ea02ceca5bd6d25a36833aa129
Lee <leep@tigerbase.com>
2017-09-03 00:29:25 -0400

    Attempt (not successfully) to set size in fceux's nl file.

--- build.ps1
+++ build.ps1
@@ -5,4 +5,17 @@ ld65.exe --config .\nes.cfg --mapfile .\popcorn.map -vm -Ln .\popcorn.lbl --obj 
 $rows = (Get-Content .\popcorn.lbl) -replace '^al 00(....) \.(.*)$', '$1#$2#' -replace '^', '$'
 $rows = $rows | Select-String -Pattern '__' -NotMatch | % { $_.Line }
 $rows = $rows | Sort-Object
+
+#for ($index = 0; $index -lt $rows.Length - 1; $index++) {
+#    $currentAddress = ($rows[$index] -match '\$(....)' | % { $Matches[1] })
+#    if ($nextAddress.SubString(0, 2) -eq "00") {
+#        $currentAddressAsInteger = [System.Convert]::ToInt32($currentAddress, 16)
+#        $nextAddress = ($rows[$index + 1] -match '\$(....)' | % { $Matches[1] })
+#        $nextAddressAsInteger = [System.Convert]::ToInt32($nextAddress, 16)
+#        $size = $nextAddressAsInteger - $currentAddressAsInteger
+#        #echo "$currentAddress/$size"
+#        $rows[$index] = "$($rows[$index].Substring(0,1))$currentAddress/$size$($rows[$index].SubString(5))"
+#    }
+#}
+
 $rows | Out-File .\bin\popcorn.nes.0.nl -Encoding ascii
